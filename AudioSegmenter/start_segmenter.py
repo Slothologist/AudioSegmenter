39790730741894322443c4f1a60626dc8990c0ed
rfeldhans <rfeldhans@techfak.uni-bielefeld.de>
2018-07-16 16:27:04 +0200

    added structure for segmenter as well as read/write to jack

---
+++ AudioSegmenter/start_segmenter.py
@@ -0,0 +1,114 @@
+"""
+Taken mostly from http://jackclient-python.readthedocs.io/en/0.4.4/examples.html
+
+Check out the Jackaudio python client package
+"""
+
+
+"""Create a JACK client that copies input audio directly to the outputs.
+
+This is somewhat modeled after the "thru_client.c" example of JACK 2:
+http://github.com/jackaudio/jack2/blob/master/example-clients/thru_client.c
+
+If you have a microphone and loudspeakers connected, this might cause an
+acoustical feedback!
+
+"""
+import sys
+import yaml
+import signal
+import os
+import jack
+import threading
+
+if sys.version_info < (3, 0):
+    # In Python 2.x, event.wait() cannot be interrupted with Ctrl+C.
+    # Therefore, we disable the whole KeyboardInterrupt mechanism.
+    # This will not close the JACK client properly, but at least we can
+    # use Ctrl+C.
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+else:
+    # If you use Python 3.x, everything is fine.
+    pass
+
+argv = iter(sys.argv)
+# By default, use script name without extension as client name:
+defaultclientname = os.path.splitext(os.path.basename(next(argv)))[0]
+clientname = next(argv, defaultclientname)
+servername = next(argv, None)
+path_to_config = next(argv, None)
+config_data = yaml.safe_load(open(path_to_config))
+
+db_min = int(config_data['db_min'])
+db_keep_alive = int(config_data['db_keep_alive'])
+time_max = int(config_data['time_max'])
+time_keep_alive = int(config_data['time_keep_alive'])
+
+
+
+client = jack.Client(clientname, servername=servername)
+
+if client.status.server_started:
+    print('JACK server started')
+if client.status.name_not_unique:
+    print('unique name {0!r} assigned'.format(client.name))
+
+event = threading.Event()
+
+
+@client.set_process_callback
+def process(frames):
+    assert len(client.inports) == len(client.outports)
+    assert frames == client.blocksize
+    for i, o in zip(client.inports, client.outports):
+        #TODO stuff
+        o.get_buffer()[:] = i.get_buffer()
+
+
+@client.set_shutdown_callback
+def shutdown(status, reason):
+    print('JACK shutdown!')
+    print('status:', status)
+    print('reason:', reason)
+    event.set()
+
+
+# create two port pairs
+for number in 1, 2:
+    client.inports.register('input_{0}'.format(number))
+    client.outports.register('output_{0}'.format(number))
+
+with client:
+    # When entering this with-statement, client.activate() is called.
+    # This tells the JACK server that we are ready to roll.
+    # Our process() callback will start running now.
+
+    # Connect the ports.  You can't do this before the client is activated,
+    # because we can't make connections to clients that aren't running.
+    # Note the confusing (but necessary) orientation of the driver backend
+    # ports: playback ports are "input" to the backend, and capture ports
+    # are "output" from it.
+
+    capture = client.get_ports(is_physical=True, is_output=True)
+    if not capture:
+        raise RuntimeError('No physical capture ports')
+
+    for src, dest in zip(capture, client.inports):
+        client.connect(src, dest)
+
+    playback = client.get_ports(is_physical=True, is_input=True)
+    if not playback:
+        raise RuntimeError('No physical playback ports')
+
+    for src, dest in zip(client.outports, playback):
+        client.connect(src, dest)
+
+    print('Press Ctrl+C to stop')
+    try:
+        event.wait()
+    except KeyboardInterrupt:
+        print('\nInterrupted by user')
+
+# When the above with-statement is left (either because the end of the
+# code block is reached, or because an exception was raised inside),
+# client.deactivate() and client.close() are called automatically.
